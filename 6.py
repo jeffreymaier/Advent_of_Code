a756baca4536fe3e935dc5a76ef344e861d050ae
jeffrey.maier <money11maier@gmail.com>
2022-12-06 09:08:14 -0700

    day 2 portion done

--- 6.py
+++ 6.py
@@ -3,19 +3,36 @@ message = list(open("6.in").read())
 # print(message)
 
 # ** Part 1 start
+# x = 0
+# y = 4
+# message_start = 4
+
+
+# for i in message:
+#     check = message[x:y]
+#     if len(check) == len(set(check)):
+#         print(message_start)
+#         break
+#     else:
+#         x += 1
+#         y += 1
+#         message_start += 1
+
+# Part 1 end **
+
+# Part 2 start
+
 x = 0
-y = 4
-message_start = 4
+y = 14
+#message_start = 14
 
 
 for i in message:
     check = message[x:y]
     if len(check) == len(set(check)):
-        print(message_start)
+        print(y)
         break
     else:
         x += 1
         y += 1
-        message_start += 1
-
-# Part 1 end **
+        #message_start += 1
